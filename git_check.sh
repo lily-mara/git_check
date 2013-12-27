e984b53e2cd52f2bd8f4bb3a59f74dae42908260
Nate Mara <natemara@gmail.com>
2013-12-26 23:54:38 -0500

    seems to be fully working now

--- git_check.sh
+++ git_check.sh
@@ -6,11 +6,21 @@ sub_dirs=`ls $git_dir | grep -v "\."`
 for i in $sub_dirs
 do
 	cd $git_dir/$i
+
 	local_hash=`git rev-parse --verify master`
 	remote_hash=`git rev-parse --verify origin/master`
 
+	need_commit=`git status --short`
 	need_push=`echo $local_hash | grep -v $remote_hash | sed s/^//`
-	if test -n "$need_push"; then
+
+	if test -n "$need_commit"; then
+		echo
+		echo
+		echo The status of $i is:
+		git status
+		echo
+		echo
+	elif test -n "$need_push"; then
 		echo $i needs a push/pull
 	fi
 
